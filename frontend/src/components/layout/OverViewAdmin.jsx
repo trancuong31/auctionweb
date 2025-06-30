8f6ae9f3d97c48165db8a3d154858393e0ed64a3
duytt <truongtrongduy10@gmail.com>
2025-06-30 17:54:00 +0700

    update admin overview

--- frontend/src/components/layout/OverViewAdmin.jsx
+++ frontend/src/components/layout/OverViewAdmin.jsx
@@ -10,8 +10,8 @@ import {
 import { useEffect, useState } from "react";
 
 const OverViewAdmin = () => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [overViewData, setOverViewData] = useState(null);
+  const [currentEditing, setCurrentEditing] = useState(null);
+  const [overViewData, setOverViewData] = useState({});
   const [userData, setUserData] = useState([]);
   const [auctionData, setAuctionData] = useState([]);
 
@@ -59,7 +59,6 @@ const OverViewAdmin = () => {
           <p className="font-semibold">Total User</p>
           <p className="text-2xl font-bold">{overViewData.total_user}</p>
           <p className="text-xl">
-            {" "}
             <FontAwesomeIcon icon={faUsers} />{" "}
           </p>
         </div>
@@ -114,66 +113,78 @@ const OverViewAdmin = () => {
               <th className="border px-2 py-1">Active</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td className="border px-2 py-1 text-center">1</td>
-              <td className="border px-2 py-1">
-                {isEditing ? (
-                  <input
-                    type="text"
-                    name="name"
-                    value={userData.username}
-                    onChange={handleChange}
-                    className="border px-2 py-1 w-full rounded"
-                  />
-                ) : (
-                  userData.name
-                )}
-              </td>
+          {userData &&
+            userData.map((user, idx) => (
+              <tbody key={user.id || idx}>
+                <tr>
+                  <td className="border px-2 py-1 text-center">{idx + 1}</td>
+                  <td className="border px-2 py-1">
+                    {currentEditing === idx ? (
+                      <input
+                        type="text"
+                        name="name"
+                        value={user.username}
+                        // onChange={handleChange}
+                        className="border px-2 py-1 w-full rounded"
+                      />
+                    ) : (
+                      user.username
+                    )}
+                  </td>
 
-              <td className="border px-2 py-1">
-                {isEditing ? (
-                  <input
-                    type="email"
-                    name="email"
-                    value={userData.email}
-                    onChange={handleChange}
-                    className="border px-2 py-1 w-full rounded"
-                  />
-                ) : (
-                  userData.email
-                )}
-              </td>
-              <td className="border px-2 py-1">{userData.created_at}</td>
-              <td className="border px-2 py-1">{userData.role}</td>
-              <td className="border px-2 py-1 flex justify-center">
-                <span className="bg-blue-500 text-white text-xs px-3 py-2 min-w-[70%] text-center rounded">
-                  Active
-                </span>
-              </td>
-              <td className="border px-2 py-1 space-x-1 text-center">
-                {isEditing ? (
-                  <button
-                    onClick={() => setIsEditing(true)}
-                    className="bg-blue-500 text-white text-xs px-3 py-2 rounded min-w-[60px]"
-                  >
-                    Save
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => setIsEditing(true)}
-                    className="bg-blue-500 text-white text-xs px-3 py-2 rounded min-w-[60px]"
-                  >
-                    Edit
-                  </button>
-                )}
-
-                <button className="bg-red-500 text-white text-xs px-3 py-2 rounded min-w-[60px]">
-                  Delete
-                </button>
-              </td>
-            </tr>
-          </tbody>
+                  <td className="border px-2 py-1">
+                    {currentEditing === idx ? (
+                      <input
+                        type="email"
+                        name="email"
+                        value={user.email}
+                        // onChange={handleChange}
+                        className="border px-2 py-1 w-full rounded"
+                      />
+                    ) : (
+                      user.email
+                    )}
+                  </td>
+                  <td className="border px-2 py-1">{user.created_at}</td>
+                  <td className="border px-2 py-1">{user.role}</td>
+                  <td className="border px-2 py-1 flex justify-center">
+                    <span className="bg-blue-500 text-white text-xs px-3 py-2 min-w-[70%] text-center rounded">
+                      Active
+                    </span>
+                  </td>
+                  <td className="border px-2 py-1 space-x-1 text-center">
+                    {currentEditing === idx ? (
+                      <>
+                        <button
+                          onClick={() => setCurrentEditing(idx)}
+                          className="bg-green-500 text-white text-xs px-3 py-2 rounded min-w-[60px]"
+                        >
+                          Save
+                        </button>
+                        <button
+                          onClick={() => setCurrentEditing(null)}
+                          className="bg-orange-500 text-white text-xs px-3 py-2 rounded min-w-[60px]"
+                        >
+                          Cancle
+                        </button>
+                      </>
+                    ) : (
+                      <>
+                        <button
+                          onClick={() => setCurrentEditing(idx)}
+                          className="bg-blue-500 text-white text-xs px-3 py-2 rounded min-w-[60px]"
+                        >
+                          Edit
+                        </button>
+                        <button className="bg-red-500 text-white text-xs px-3 py-2 rounded min-w-[60px]">
+                          Delete
+                        </button>
+                      </>
+                    )}
+                  </td>
+                </tr>
+              </tbody>
+            ))}
         </table>
       </div>
 
@@ -203,26 +214,35 @@ const OverViewAdmin = () => {
               <th className="border px-2 py-1">#</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td className="border px-2 py-1 text-center">1</td>
-              <td className="border px-2 py-1">{auctionData.title}</td>
-              <td className="border px-2 py-1">{auctionData.start_time}</td>
-              <td className="border px-2 py-1">{auctionData.end_time}</td>
-              <td className="border px-2 py-1">{auctionData.starting_price}</td>
-              <td className="border px-2 py-1">{auctionData.highest_amount}</td>
-              <td className="border px-2 py-1">
-                {auctionData.status === 0
-                  ? "Upcoming"
-                  : auctionData.status === 1
-                  ? "Ongoing"
-                  : "Ended"}
-              </td>
-              <td className="border px-2 py-1 text-blue-500 underline cursor-pointer">
-                View
-              </td>
-            </tr>
-          </tbody>
+          {auctionData &&
+            auctionData.map((auction, idx) => {
+              let statusText = "Ended";
+              if (auction.status === 0) {
+                statusText = "Upcoming";
+              } else if (auction.status === 1) {
+                statusText = "Ongoing";
+              }
+              return (
+                <tbody key={auction.id || idx}>
+                  <tr>
+                    <td className="border px-2 py-1 text-center">{idx + 1}</td>
+                    <td className="border px-2 py-1">{auction.title}</td>
+                    <td className="border px-2 py-1">{auction.start_time}</td>
+                    <td className="border px-2 py-1">{auction.end_time}</td>
+                    <td className="border px-2 py-1">
+                      {auction.starting_price}
+                    </td>
+                    <td className="border px-2 py-1">
+                      {auction.highest_amount}
+                    </td>
+                    <td className="border px-2 py-1">{statusText}</td>
+                    <td className="border px-2 py-1 text-blue-500 underline cursor-pointer">
+                      View
+                    </td>
+                  </tr>
+                </tbody>
+              );
+            })}
         </table>
       </div>
     </div>
