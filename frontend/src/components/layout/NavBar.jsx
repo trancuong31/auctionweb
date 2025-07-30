b3b5ae44fd0900e09de79ed7831ce73fb99bb12e
ttduytt <ttdzoraidea@gmail.com>
2025-07-30 07:28:34 +0700

    editAuction

--- frontend/src/components/layout/NavBar.jsx
+++ frontend/src/components/layout/NavBar.jsx
@@ -62,40 +62,39 @@ function NavBar() {
 
   return (
     <>
-    <nav className="nav-wrapper">
-      <div className="nav-left">
-        <NavLink to="/" className="nav-link">
-          {t("home", "Home")}
-        </NavLink>
-        <NavLink to="/auctions/search" className="nav-link">
-          {t("auction", "Auction")}
-        </NavLink>
-        <NavLink to="/about" className="nav-link">
-          {t("information", "Information")}
-        </NavLink>
-        <NavLink to="/history" className="nav-link">
-          {t("history", "History")}
-        </NavLink>
-        <NavLink to="/guide" className="nav-link">
-          {t("tutorial", "Tutorial")}
-        </NavLink>
-        <NavLink to="/contact" className="nav-link">
-          {t("contact", "Contact")}
-        </NavLink>
-        <NavLink to="/policy" className="nav-link">
-          {t("rule", "Rule")}
-        </NavLink>
-        {(user?.role === "admin" || user?.role === "super_admin") && (
-          <NavLink to="/admin" className="nav-link">
-            {t("dashboard", "Dashboard")}
+      <nav className="nav-wrapper">
+        <div className="nav-left">
+          <NavLink to="/" className="nav-link">
+            {t("home", "Home")}
           </NavLink>
-        )}
-      </div>
-      <div className="nav-right">
-        {user ? (
-          <>
-            
-            <span
+          <NavLink to="/auctions/search" className="nav-link">
+            {t("auction", "Auction")}
+          </NavLink>
+          <NavLink to="/about" className="nav-link">
+            {t("information", "Information")}
+          </NavLink>
+          <NavLink to="/history" className="nav-link">
+            {t("history", "History")}
+          </NavLink>
+          <NavLink to="/guide" className="nav-link">
+            {t("tutorial", "Tutorial")}
+          </NavLink>
+          <NavLink to="/contact" className="nav-link">
+            {t("contact", "Contact")}
+          </NavLink>
+          <NavLink to="/policy" className="nav-link">
+            {t("rule", "Rule")}
+          </NavLink>
+          {(user?.role === "admin" || user?.role === "super_admin") && (
+            <NavLink to="/admin" className="nav-link">
+              {t("dashboard", "Dashboard")}
+            </NavLink>
+          )}
+        </div>
+        <div className="nav-right">
+          {user ? (
+            <>
+              <span
                 className="user-greeting nav-link"
                 onClick={() => setShowAccountInfo(false)}
                 style={{ cursor: "pointer" }}
@@ -154,19 +153,17 @@ function NavBar() {
           </div>
         </div>
       )}
-
+      <AuctionHistory
+        isOpen={showAuctionHistory}
+        onClose={() => setShowAuctionHistory(false)}
+      />
       {showAuctionHistory && (
         <div
           onClick={() => setShowAuctionHistory(false)}
           aria-modal="true"
           role="dialog"
         >
-          <div onClick={(e) => e.stopPropagation()}>
-            <AuctionHistory
-              isOpen={showAuctionHistory}
-              onClose={() => setShowAuctionHistory(false)}
-            />
-          </div>
+          <div onClick={(e) => e.stopPropagation()}></div>
         </div>
       )}
     </>
